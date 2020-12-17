374f829a50e9b43c791143af05dbfbc0d7223fd4
P.Y. Laligand <py.laligand@gmail.com>
2020-12-16 18:06:29 -0800

    Template for tests (#12)

---
+++ base/tests.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+import unittest
+
+
+class TestXx(unittest.TestCase):
+
+    def test_something(self):
+        self.assertTrue(False)
+
+
+if __name__ == "__main__":
+    unittest.main()
